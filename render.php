aefa08ee4af065021e09fa56dcdc01dbc91e3872
Mattias Geniar <m@ttias.be>
2016-11-21 10:48:57 +0100

    Fix: only include HTTPS links, never include HTTP links

--- render.php
+++ render.php
@@ -24,8 +24,8 @@ if ($_POST['type'] == 'html') {
 
   /* Start HTML content */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
 <head><meta charset="utf-8"><!-- utf-8 works for most cases -->
   <meta name="viewport" content="width=device-width"><!-- Forcing initial-scale shouldn't be necessary -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge"><!-- Use the latest (edge) version of IE rendering engine -->
@@ -37,7 +37,7 @@ if ($_POST['type'] == 'html') {
                 font-family: sans-serif !important;
             }
         </style>
-    <![endif]--><!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ --><!--[if !mso]><!--><!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> --><!--<![endif]--><!-- Web Font / @font-face : END --><!-- CSS Reset -->
+    <![endif]--><!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks.  --><!--[if !mso]><!--><!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> --><!--<![endif]--><!-- Web Font / @font-face : END --><!-- CSS Reset -->
   <style type="text/css">
         /* What it does: Remove spaces around the email design added by some email clients. */
         /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
